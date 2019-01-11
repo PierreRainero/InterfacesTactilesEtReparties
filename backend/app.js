1d4a05806c9544e94388495b2f6a409d76ae74b9
Julien Maureille <julien.maureille@etu.unice.fr>
2019-01-11 11:20:28 +0100

    Setup video-projector

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(8282);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
---
+++ video_projector/services/socket.js
@@ -0,0 +1,7 @@
+
+
+var socket = io('http://localhost:8282');
+socket.on('news', function (data) {
+    console.log(data);
+    //socket.emit('my other event', { my: 'data' });
+});
