1252e280454efbcd2f4c7f575daa29c872a33405
Julien Maureille <julien.maureille@etu.unice.fr>
2019-01-11 11:24:58 +0100

    Communication between backend and videoproj

--- video_projector/services/socket.js
+++ video_projector/services/socket.js
@@ -3,5 +3,5 @@
 var socket = io('http://localhost:8282');
 socket.on('news', function (data) {
     console.log(data);
-    //socket.emit('my other event', { my: 'data' });
+    socket.emit('my other event', { my: 'data' });
 });
