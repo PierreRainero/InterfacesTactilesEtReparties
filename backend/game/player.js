c00820bc841b7c60fcb2c28018a2456a22d5585a
JulienMaureille <julien.maureille@live.fr>
2019-01-25 15:48:06 +0100

    Saut de haies

--- backend/game/player.js
+++ backend/game/player.js
@@ -2,10 +2,19 @@ module.exports = class Player {
   constructor(id, state) {
     this.id = id;
     this.state = state;
+    this.progress = 0;
   }
 
   setWatchCaptor(deviceId, dataSharing) {
     this.deviceId = deviceId;
     this.allowDataSharing = dataSharing;
+  }
+
+  isApproachingHurdle(map){
+    for (let hurdle in map.hurdles){
+      if(this.progress > hurdle - 2 && this.progress < hurdle){
+
+      }
+    }
   }
 }
