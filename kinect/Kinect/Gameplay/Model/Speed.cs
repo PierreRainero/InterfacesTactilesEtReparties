29686d6176312dcdcc129804f1f3a38800927a15
Pierre Rainero <pierre.rainero@etu.unice.fr>
2019-02-07 12:22:34 +0100

    Increase players speed

--- kinect/Kinect/Gameplay/Model/Speed.cs
+++ kinect/Kinect/Gameplay/Model/Speed.cs
@@ -66,7 +66,7 @@ namespace Kinect.Gameplay.Model
 
             indexer = 0;
             float averageRunningSpeedFactor = (float)0.857836;
-            value = SpeedCorrector((rigthSum + leftSum) / 2 * 18 * averageRunningSpeedFactor);
+            value = SpeedCorrector((rigthSum + leftSum) / 2 * 19 * averageRunningSpeedFactor);
 
             return value;
         }
